6eed4efa563d8881bd3952db2b80997625381e18
Bhanu Jain <bhanujain88@gmail.com>
2016-03-04 15:57:57 -0500

    Adding Registration

--- public/app/views/users/register.controller.js
+++ public/app/views/users/register.controller.js
@@ -7,7 +7,32 @@
         .module('GluecApp')
         .controller('RegisterController', RegisterController);
 
-    function RegisterController($location, $scope, $rootScope) {
-        $scope.$location = $location;
+    function RegisterController(UserService, $scope, $rootScope, $location) {
+
+        //Event Handlers Decelerations
+        $scope.register = Register;
+
+        //Event Handlers Implementations
+        function Register() {
+
+            var form_user = {
+                username: $scope.form.user_name,
+                password: $scope.form.user_password,
+                repeat_password: $scope.form.user_password_repeat,
+                email: $scope.form.user_email
+            };
+
+            UserService.createUser(form_user, userRegisterCallback)
+
+            function userRegisterCallback(response) {
+                if (response != null) {
+                    //Storing the user in the Root Scope
+                    $rootScope.user = response;
+                    // Navigating to the Profile Page of this particular User
+                    $location.url("/profile/");
+                }
+            };
+        };
+
     };
 })();
