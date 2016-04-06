e0151fc88334e6fd36f76e967655c98597a1ab68
Bhanu Jain <bhanujain88@gmail.com>
2016-04-06 18:03:49 -0400

    Adding Mongoose Config

--- server.js
+++ server.js
@@ -9,10 +9,24 @@ var uuid = require("node-uuid");
 var app = express();
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+var mongoose = require("mongoose");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(multer());
+
+// create a default connection string
+var connectionString = 'mongodb://127.0.0.1:27017/Gluec';
+
+// use remote connection string
+// if running in remote server
+if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
 
 app.use(express.static(__dirname + '/public'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
