9940305615458398b3d43e55d002bbdf6fba895b
ronnygeo <ronnygeomatt@gmail.com>
2016-04-12 18:26:43 -0400

    adding reply button to messages.

--- public/app/views/messages/messages.controller.js
+++ public/app/views/messages/messages.controller.js
@@ -2,17 +2,34 @@
     angular.module("GluecApp")
         .controller('MessageController', MessageController);
 
-    MessageController.$inject = ['$routeParams', 'MessageService'];
+    MessageController.$inject = ['$routeParams', 'MessageService', '$rootScope'];
 
-    function MessageController($routeParams, MessageService) {
+    function MessageController($routeParams, MessageService, $rootScope) {
 
         var vm = this;
+        var fromUserId = $rootScope.user._id;
 
         var userId = $routeParams.id;
 
         angular.element(document).ready(function () {
             $('.collapsible').collapsible({
                 accordion : false // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+            });
+
+            $('.modal-trigger').leanModal({
+                dismissible: true, // Modal can be dismissed by clicking outside of the modal
+                opacity: .5, // Opacity of modal background
+                in_duration: 300, // Transition in duration
+                out_duration: 200, // Transition out duration
+                ready: function() {
+                    vm.message = {};
+                    vm.message.from = fromUserId;
+                    vm.message.subject = "";
+                    vm.message.to = userId;
+                }, // Callback for Modal open
+                complete: function() {
+                    MessageService.createMessage(userId, vm.message).then(function(data) {
+                    });} // Callback for Modal close
             });
         });
         MessageService.findMessagesByUser(userId).then(function (data) {
