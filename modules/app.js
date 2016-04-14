906332d26c51ce6e65c7b5c8bc9b90ac8508ab40
Bhanu Jain <bhanujain88@gmail.com>
2016-04-14 19:26:24 -0400

    changing cartirge

--- modules/app.js
+++ modules/app.js
@@ -26,7 +26,5 @@ module.exports = function (app, request, q, upload, mongoose, uuid) {
     var listingModel = require("./models/listing/listing.model")(listingDB, ebayListingDB, q);
     require("./services/listing/listing.service.server")(app, q, listingModel, categoryModel, ebayAPIClient, upload, amazonAPIClient, uuid);
 
-
     //require("./services/image/clarfai.image.service")();
-    
 };
