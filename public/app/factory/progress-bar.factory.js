6a78d37df04a8a3574dda31d8ab39fc126527910
ronnygeo <ronnygeomatt@gmail.com>
2016-04-15 18:20:53 -0400

    Refactoring dashboard.

--- public/app/factory/progress-bar.factory.js
+++ public/app/factory/progress-bar.factory.js
@@ -19,7 +19,7 @@
         function showProgressBar() {
             $rootScope.progressbar = ngProgressFactory.createInstance();
             $rootScope.progressbar.setHeight('4px');
-            $rootScope.progressbar.setColor('#000000');
+            $rootScope.progressbar.setColor('#333333');
             $rootScope.progressbar.start();
         }
 
