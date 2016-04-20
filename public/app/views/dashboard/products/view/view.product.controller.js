eb9956a90859cd5a81f77768664f8bf6f55f0329
Bhanu Jain <bhanujain88@gmail.com>
2016-04-20 00:50:17 -0400

    Sell Flow From Product Complete

--- public/app/views/dashboard/products/view/view.product.controller.js
+++ public/app/views/dashboard/products/view/view.product.controller.js
@@ -2,16 +2,38 @@
     angular.module('GluecApp')
         .controller('ViewProductController', ViewProductController);
 
-    ViewProductController.$inject = ['ProductService', '$route'];
+    ViewProductController.$inject = ['ProductService', '$route', '$rootScope', '$location'];
 
-    function ViewProductController(ProductService, $route) {
+    function ViewProductController(ProductService, $route, $rootScope, $location) {
         var vm = this;
+        vm.deleteProduct = deleteProduct;
+        vm.createListing = createListing;
         var prodId = $route.current.params.prodId;
-        
-        ProductService.findProductById(prodId).then(function (data) {
-            // console.log(data.data);
-            vm.product = data.data;
-        });
+
+        function init() {
+            ProductService.findProductById(prodId).then(function (data) {
+                // console.log(data.data);
+                vm.product = data.data;
+            });
+        }
+
+        init();
+
+        function createListing(product) {
+            console.log("createListing");
+            if (!product) {
+                return
+            }
+            $rootScope.prodListing = product;
+            $location.url("/listing/create/prod/");
+        }
+
+        function deleteProduct(prodId) {
+            console.log("deleteProduct");
+            ProductService.deleteProduct($rootScope.user._id, prodId).then(function () {
+                $location.url("/dashboard/products/");
+            });
+        }
     }
 
 })();
